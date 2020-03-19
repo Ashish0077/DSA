ed9f402a5b58b53cca8ab23471ca0ff5bb03bda3
Ashish0077 <ashish007722@gmail.com>
2020-03-20 03:25:08 +0530

    [DynamicArray.cpp] emplace, pop insert at the end and delete from the back, if more size is required it reallocates memory
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

--- DynamicArray.cpp
+++ DynamicArray.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 template<typename T>
@@ -6,20 +8,84 @@ class DynamicArray {
     private:
         T* arr;
         int count;
-        int size;
+        int _size;
 
     public:
         DynamicArray() {
-            T* arr = new T[1];
-            count = 0;
-            size = 1;
+            arr = new T[1];
+            count = 0;  // points at the next index to insert element
+            _size = 1; // actual size of the array (capacity)
         }
 
-        void insert(const int data) {
-            
+        ~DynamicArray() {
+            delete[] arr;
         }
+
+        // inserts at the back
+        void emplaceBack(const T data) {
+            // if array is full reallocating space
+            if(count == _size) {
+                _size = _size * 2;
+                T* newArr = new T[_size];
+                for(int i = 0; i < count; i++) {
+                    newArr[i] = arr[i];
+                }
+                delete[] arr;
+                arr = NULL;
+                arr = newArr;
+            }
+
+            // emplacing data at the back
+            arr[count] = data;
+            count++;
+        }
+
+        // alternative to emplaceBack
+        void pushBack(const T data) {
+            emplaceBack(data);
+        }
+
+        // remove elements from the back
+        void popBack() {
+            // count cant go negative
+            if(count > 0)
+                count--;
+        }
+
+        T operator[](int i) {
+            return arr[i];
+        }
+
+        int capacity() {
+            return _size;
+        }
+
+        int size() {
+            return count;
+        }
+
 };
 
 int main() {
+
+    // Test Code
+    DynamicArray<int> a;
+    cout << a.capacity() << endl;
+    a.emplaceBack(2);
+    for (int i = 0; i < a.size(); i++) {
+        cout << a[i] << " ";
+    }
+    cout << "capacity : " <<  a.capacity() << endl;
+    a.pushBack(3);
+    for (int i = 0; i < a.size(); i++) {
+        cout << a[i] << " ";
+    }
+    a.popBack();
+    cout << "capacity : " <<  a.capacity() << endl;
+    a.pushBack(4);
+    for (int i = 0; i < a.size(); i++) {
+        cout << a[i] << " ";
+    }
+    cout << "capacity : " <<  a.capacity() << endl;
     return 0;
 }
