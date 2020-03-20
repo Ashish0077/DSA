0b53c6fdc133e20328a63a38f641b8f7a3e46990
Ashish0077 <ashish007722@gmail.com>
2020-03-20 20:00:28 +0530

    [SinglyLinkedList.cpp] insert(), popFront() implemented proper Iterator and othe functionality of singly linkedlist
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

--- LinkedList/SinglyLinkedList.cpp
+++ LinkedList/SinglyLinkedList.cpp
@@ -12,7 +12,9 @@ class SinglyList {
 
         // internal head node
         Node* head;
+        // internal tail node (this is used to optimize the insertion operation)
         Node* tail;
+        // this keeps track of number of nodes in the list
         int _size;
 
         Node* createNewNode() {
@@ -50,6 +52,13 @@ class SinglyList {
                     return *this;
                 }
 
+                Iterator &operator+(int k) {
+                    for(int i = 0; i < k; i++) {
+                        it = it->next;
+                    }
+                    return *this;
+                }
+
                 bool operator!=(const Iterator &other) const{
                     return it != other.it;
                 }
@@ -66,7 +75,6 @@ class SinglyList {
         Iterator end() {
             return Iterator(NULL);
         }
-
 
         void emplaceBack(const T &data) {
             Node* newNode = createNewNode();
@@ -88,8 +96,6 @@ class SinglyList {
             _size++;
         }
 
-
-
         void popBack() {
             Node* it = head;
             while (it->next != tail) {
@@ -99,6 +105,39 @@ class SinglyList {
             it->next = NULL;
             delete tail;
             tail = it;
+            _size--;
+        }
+
+        void popFront() {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            _size--;
+        }
+
+        void insert(Iterator at,const T &data) {
+            
+            if (begin() == at) {
+                emplaceFront(data);
+                return;
+            } else if(end() == at) {
+                emplaceBack(data);
+                return;
+            }
+
+            Node* newNode = createNewNode();
+            newNode->data = data;
+            auto it = begin();
+            while((*it)->next != (*at)) {
+                it++;
+            }
+            newNode->next = (*at);
+            (*it)->next = newNode;
+            _size++;
+        }
+
+        int size() const {
+            return _size;
         }
  };
 
@@ -106,25 +145,63 @@ int main() {
 
     SinglyList<int> a;
 
+    cout << "Size : " << a.size() << endl;
+
     a.emplaceBack(1);
     a.emplaceBack(2);
     a.emplaceBack(3);
+    
+    // 0 indexed
+    a.insert(a.begin() + 1, 22);
+
+    // Method 1: iterating Linked List
+    auto it = a.begin();
+    for(int i = 0; i < a.size(); i++) {
+        cout << (*it)->data << " -> ";
+        (*it) = (*it)->next;
+    }
+    cout << "NULL\n";
+    
+    cout << "Size : " << a.size() << endl;
+
     a.emplaceBack(4);
     a.emplaceBack(5);
 
+    // Method 2: Iterating Linked List
     for(auto i = a.begin(); i != a.end(); i++) {
         cout << (*i)->data << " -> ";
     }
+    cout << "NULL\n";
 
-    cout << endl;
+    a.emplaceFront(6);
+    
+    for(auto i = a.begin(); i != a.end(); i++) {
+        cout << (*i)->data << " -> ";
+    }
+    cout << "NULL\n";
+
+    cout << "Size : " << a.size() << endl;
+
     a.popBack();
     a.popBack();
-    a.popBack();
-    a.popBack();
+
+    cout << "Size : " << a.size() << endl;
+    
+    // Method 3: Iterating Linked List
+    for(auto i : a) {
+        cout << i->data << " -> ";
+    }
+    cout << "NULL\n";
+
+    a.popFront();
+    a.popFront();
+    
+    cout << "Size : " << a.size() << endl;
 
     for(auto i : a) {
         cout << i->data << " -> ";
     }
+    cout << "NULL\n";
 
     return 0;
 }
