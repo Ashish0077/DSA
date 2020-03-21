eb222487c1e4fd10a87f2a1fbb130a52d190341b
Ashish0077 <ashish007722@gmail.com>
2020-03-21 15:59:41 +0530

    [DynamicArray.cpp] update added plenty of comments to make the code more understandable
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

--- DynamicArray/DynamicArray.cpp
+++ DynamicArray/DynamicArray.cpp
@@ -4,13 +4,14 @@ using namespace std;
 template<typename T>
 class DynamicArray {
     private:
+        // internal array pointer for dynamic allocation
         T* arr;
         int count;
         int _size;
 
     public:
         DynamicArray() {
-            arr = new T[1];
+            arr = new T[1]; // Intially the size of the array will be 1
             count = 0;  // points at the next index to insert element
             _size = 1; // actual size of the array (capacity)
         }
@@ -19,6 +20,10 @@ class DynamicArray {
             delete[] arr;
         }
 
+        /*
+            Iterator Class:
+            All functinality for iterating the array
+        */
         class Iterator {
             private:
                 const DynamicArray<T> *pArr;
@@ -29,40 +34,74 @@ class DynamicArray {
                     index = i;
                 }
 
+                /*
+                    Overloading * operator to make internal data accessible
+                */
                 const T &operator*() const{
                     return pArr->operator[](index);
                 }
 
+                /*
+                    Overloading prefix ++ operator
+                    this makes the iterator to move to next index
+                */
                 Iterator &operator++() {
                     index++;
                     return *this;
                 }
 
-                // overloading postfix operator (dummy parameter required)
+                /*
+                    Overloading postfixx ++ operator
+                    this makes the iterator to move to next index
+                */
                 Iterator &operator++(int k) {
                     Iterator it(pArr, index);
                     index++;
                     return it;
                 }
 
+                /*
+                    Overloading != operator
+                    this is used for comparision
+                    returns true when not equal else false
+                */
                 bool operator!=(const Iterator &other) const {
                     return index != other.index;
                 }
 
+                /*
+                    Overlaading == operator
+                    this is used for comparision
+                    return true when equal else false
+                */
                 bool operator==(const Iterator &other) const {
                     return index == other.index;
                 }
         };
 
+        /*
+            This function is used to get a Iterator
+            from the begining of the array
+            begin() and end() helps in implementing for-each loop
+        */
         Iterator begin() const{
             return Iterator(this, 0);
         }
-
+        
+        /*
+            This function is used to get a Iterator to end of the list
+            begin() and end() helps in implementing for-each loop
+        */
         Iterator end() const{
             return Iterator(this , count);
         }
 
-        // inserts at the back
+        /*
+            This function is used to insert elements
+            at the back/end of the list
+            Time Complexity : O(1) on average
+            when the array reallocate its o(n)
+        */
         void emplaceBack(const T &data) {
             // if array is full reallocating space
             if(count == _size) {
@@ -98,14 +137,17 @@ class DynamicArray {
             return arr[i];
         }
 
+        // returns the element at index i
         T &operator[](int i) const{
             return arr[i];
         }
 
+        // returns the orignal size of the array
         int capacity() const{
             return _size;
         }
 
+        // returns the number of elements
         int size() const{
             return count;
         }
