8f53be8e6f1bb20374c6d7049ed2df605fa2e63c
Ashish0077 <ashish007722@gmail.com>
2020-03-29 00:16:56 +0530

    [DirectedGraph.cpp] update
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

--- Graphs/DirectedGraph.cpp
+++ Graphs/DirectedGraph.cpp
@@ -2,20 +2,19 @@
 #include <forward_list>
 using namespace std;
 
-template<typename T>
 class DirectedGraph {
     private:
         int vertices;
-        forward_list<pair<int, T>>* arr;
+        forward_list<int>* arr;
     public:
         DirectedGraph(int v) {
             vertices = v;
-            arr = new forward_list<pair<int, T>>[vertices];
+            arr = new forward_list<int>[vertices];
         }
 
-        void insertEdge(int source, int destination, T data) {
+        void insertEdge(int source, int destination) {
             if (source < vertices && destination < vertices) {
-                arr[source].emplace_front(destination, data);
+                arr[source].emplace_front(destination);
             }
         }
 
@@ -24,19 +23,29 @@ class DirectedGraph {
             for(int i = 0; i < vertices; i++) {
                 cout << "|" << i << "| : ";
                 for(auto it: arr[i]) {
-                    cout << "[" << it.first << ", " << it.second << "] -> ";
+                    cout << "[" << it << "] -> ";
                 }
                 cout << "null" << endl;
             }
         }
 };
 
+enum {
+    Delhi,
+    Mumbai,
+    Kolkata,
+    Banglore,
+    Kashmir
+};
+
 int main() {
-    DirectedGraph<string> g(4);
-    g.insertEdge(0, 1, "Delhi");
-    g.insertEdge(0, 2, "Noida");
-    g.insertEdge(1, 3, "Gurgaon");
-    g.insertEdge(2, 3, "Ghaziabad");
+    DirectedGraph g(4);
+    g.insertEdge(Delhi, Mumbai);
+    g.insertEdge(Delhi, Banglore);
+    g.insertEdge(Mumbai, Banglore);
+    g.insertEdge(Banglore, Mumbai);
+    g.insertEdge(Banglore, Kolkata);
+    g.insertEdge(Kolkata, Mumbai);
     cout << endl;
     g.display();
     return 0;
