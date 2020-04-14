7235b678e67b1c82ce0cc1a058693c4fba26cfb5
Ashish0077 <ashish007722@gmail.com>
2020-04-15 02:31:17 +0530

    [BinaryTree.cpp] Iterative DFS schemes
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

--- Trees/BinaryTree.cpp
+++ Trees/BinaryTree.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stack>
+
 using namespace std;
 
 template<typename T>
@@ -45,11 +47,46 @@ class BinaryTree {
             }
         }
 
+        static void preOrderTraversal(Node* root) {
+            if(root == nullptr)
+                return;
+            
+            stack<Node*> nodes;
+            nodes.push(root);
+
+            while(!nodes.empty()) {
+                Node* current = nodes.top();
+                nodes.pop();
+                cout << current->getData() << " ";
+                if(current->getLeft() != nullptr) nodes.push(current->getRight());
+                if(current->getRight() != nullptr) nodes.push(current->getLeft());
+            }
+        }
+
         static void inOrderTraversalRecursive(Node* root) {
             if(root != nullptr) {
                 inOrderTraversalRecursive(root->getLeft());
                 cout << root->getData() << " ";
                 inOrderTraversalRecursive(root->getRight());
+            }
+        }
+
+        static void inOrderTraversal(Node* root) {
+            stack<Node*> nodes;
+            while(root != nullptr || !nodes.empty()) {
+                
+                // Pushing all the left nodes onto the stack
+                while(root != nullptr) {
+                    nodes.push(root);
+                    root = root->getLeft();
+                }
+
+                // as the root will be null now
+                root = nodes.top();
+                nodes.pop();
+                cout << root->getData() << " ";
+                
+                root = root->getRight();
             }
         }
 
@@ -59,6 +96,38 @@ class BinaryTree {
                 postOrderTraversalRecursive(root->getRight());
                 cout << root->getData() << " ";
             }
+        }
+
+        static void postOrderTraversal(Node* root) {
+            stack<Node*> nodes;
+            while(!nodes.empty() || root != nullptr) {
+                
+                // pushing all the left nodes on the stack
+                while(root != nullptr) {
+                    if(root->getRight() != nullptr) nodes.push(root->getRight());
+                    nodes.push(root);
+                    root = root->getLeft();
+                }
+
+                //currently root is null
+                root = nodes.top();
+                nodes.pop();
+
+                if(nodes.empty()) {
+                    cout << root->getData() << " ";
+                    break;
+                }
+
+                if(root->getRight() != nullptr && nodes.top() == root->getRight()) {
+                    nodes.pop(); // popped right node form the stack
+                    nodes.push(root); // pushed root back
+                    root = root->getRight(); // changed root to right so that right child is processed first
+                } else {
+                    cout << root->getData() << " ";
+                    root = nullptr;
+                }
+            }
+
         }
 
         static int getHeight(Node* root) {
@@ -88,6 +157,13 @@ class BinaryTree {
 
 int main () {
 
+    // constructing a tree
+    /*  
+                        1
+                2               3     
+            4      5        6       7
+          8  9  10  11   13  14  15  16
+    */
     auto root = new BinaryTree<int>::Node(1);
     root->setLeft(2);
     root->setRight(3);
@@ -107,15 +183,21 @@ int main () {
     cout << "Height of the Tree : " << BinaryTree<int>::getHeight(root) << endl;
 
     // Depth First Search
-    cout << "InOrder Recursive\n";
+    cout << "\nInOrder Recursive\n";
     BinaryTree<int>::inOrderTraversalRecursive(root);
-    cout << "\nPostOrder Recursive\n";
+    cout << "\nInOrder Iterative\n";
+    BinaryTree<int>::inOrderTraversal(root);
+    cout << "\n\nPostOrder Recursive\n";
     BinaryTree<int>::postOrderTraversalRecursive(root);
-    cout << "\nPreOrder Recursive\n";
+    cout << "\nPostOrder Iterative\n";
+    BinaryTree<int>::postOrderTraversal(root);
+    cout << "\n\nPreOrder Recursive\n";
     BinaryTree<int>::preOrderTraversalRecursive(root);
+    cout <<"\nPreOrder Iterative\n";
+    BinaryTree<int>::preOrderTraversal(root);
 
     // Breadth First Search
-    cout << "\nLevelOrder Recursive\n";
+    cout << "\n\nLevelOrder Recursive\n";
     BinaryTree<int>::levelOrderTraversalRecursive(root);
     cout << "\n";
     
